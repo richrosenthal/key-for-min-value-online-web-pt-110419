fe536b58d1c721af54faa9cb5fa3820fe7d7cc01
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-03 02:02:45 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,44 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  array = []
+  count = 0
+   name_hash.collect do |key, data|
+    array << data
+  end
+ 
 
+  i = 0 
+  y = i + 1 
+  n = array.length
+  swap = true 
+  
+  while swap do
+  swap = false 
+  (n-1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i+ 1] = array[i + 1], array[i]
+        swap = true
+      end
+  end
 end
+  new_count = 0 
+  name_hash.each do |key, data|
+    if data == array[new_count]
+      return key 
+    end
+     
+  end 
+  return nil 
+end 
+
+
+  
+    
+  
+
+
+
+
+ 
+  
