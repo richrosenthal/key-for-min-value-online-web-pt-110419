508fa7d627ebc328cebbcccfc6e374d8086f7177
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-03 01:38:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,20 +8,6 @@ def key_for_min_value(name_hash)
     array << data
   end
 
-def array_sort(array)  
-   i = 0 
-   y = i + 1 
-   while i < array.length  
-      if array[i] > array[y] 
-        array.insert(i, array.delete_at(y+1))
-      elsif array[i] < array[y]
-        array.insert(i, array.delete_at(y-1))
-      else
-        i += 1 
-        y += 1
-      
-   end
-   array
-  end 
+
  
   
