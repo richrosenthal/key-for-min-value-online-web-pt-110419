e15dffbe03ec06c2af64b59e449d1c53951f6587
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-03 01:09:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  array = []
+  count = 0
+   name_hash.collect do |key, data|
+    array << data
+end
+count = 0
+  array.each do |number|
+   if array[count - 1] < array[count]
+     array[count].unshift
+   end
+end
+array
 end
