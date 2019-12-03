1529884e1d761fecc9419197823f04f183967ce0
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-03 01:12:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   count = 0
    name_hash.collect do |key, data|
     array << data
-end
+  end
+  
+  array.each do 
 count = 0
   array.each do |number|
    if array[count - 1] < array[count]
